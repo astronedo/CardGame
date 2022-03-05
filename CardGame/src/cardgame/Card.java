70e0ee6737558bd2c937fa91d928f5cd479792c2
astro <astro@192.168.2.46>
2022-03-04 21:03:25 -0500

    2022-03-04 update

---
+++ CardGame/src/cardgame/Card.java
@@ -0,0 +1,8 @@
+package cardgame;
+
+public class Card {
+    enum Value{ace, two, three, four, five, six}
+    enum Suit{Diamonds, Spades, Hearts, Clubs}
+    
+    
+}
--- CardGame/src/cardgame/Main.java
+++ CardGame/src/cardgame/Main.java
@@ -1,9 +1,34 @@
 package cardgame;
 
+import static java.lang.Double.NaN;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Find-A-Match (must be 18 or older)");
+        Player player=new Player();
+        //------------------------------------NAME
+        String name;
+        do{
+        System.out.println("Enter Name: ");
+        name=input.nextLine();
+        }while("".equals(name));
+        player.setName(name);
+        //------------------------------------AGE
+        int age;
+           try{
+           System.out.println("Enter Age: ");
+           age=input.nextInt();
+           player.setAge(age);
+           }catch(Exception e){
+               System.out.println("Incorrect Input");
+           }
+        //------------------------------------GAME
+        
+        
+        
     }
     
 }
